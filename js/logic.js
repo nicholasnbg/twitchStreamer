3103a99209a12866a47c1e53249dbcf82dd83eaf
nbg <nicholasnbg@gmail.com>
2017-07-19 13:37:18 +1000

    finished insertStream function

--- js/logic.js
+++ js/logic.js
@@ -3,8 +3,7 @@ $(document).ready(function(){
   //variables
   var clientID = '?client_id=z2yjt6dgs59kua030e1m9pa1i9rifx';
   var streams_api = "https://api.twitch.tv/kraken/streams/";
-  var channels = ["ESL_SC2", "OgamingSC2", "Killingbird", "faketwitchjiberish"];
-  var streamData = [];
+  var channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
   var streamCount = 3;
   var rows = 1;
 
@@ -35,6 +34,7 @@ $(document).ready(function(){
       'name:':'',
       'link':'',
       'status':'',
+      'linkText':'',
     }
 
     //offline is currently running both ifs, fix this
@@ -44,6 +44,7 @@ $(document).ready(function(){
       streamInfo.status = "offline"
       streamInfo.name = channels[i];
       streamInfo.link = data._links.channel;
+      streamInfo.linkText = '<a href="https://www.twitch.tv/'+streamInfo.name+'/videos/all">Go to channel</a>';
       streamInfo.logo = "https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/ProhibitionSign2.svg/1200px-ProhibitionSign2.svg.png";
       //run function to append row
       insertStream(streamInfo);
@@ -54,6 +55,7 @@ $(document).ready(function(){
         streamInfo.status="online";
         streamInfo.text = name  + " is currently playing " + data.stream.game;
         streamInfo.logo =  data.stream.channel.logo;
+        streamInfo.linkText = '<a href="">Watch here</a>';
         streamInfo.link = data.stream._links.self;
         //run function to insert row
       insertStream(streamInfo);
@@ -62,12 +64,12 @@ $(document).ready(function(){
   }
 
   function insertStream(streamObj){
-    var rowHTML = '<div class="row '+streamObj.status+'"><img src="'+streamObj.logo+'" alt=""><span class= "streamName">'+streamObj.name+'</span><span class="streamText">'+streamObj.text+'</span></div>';
+    var logoHTML = '<div class="col-xs-2"><img src="'+streamObj.logo+'" alt=""></div>';
+    var streamHTML = '<div class="col-xs-3" class="streamName">'+ streamObj.name +'</div>';
+    var textHTML =  '<div class="col-xs-5" class="streamText">'+ streamObj.text +'</div>';
+    var linkHTML =  '<div class="col-xs-2" class="linkText">'+ streamObj.linkText +'</div>';
+    var rowHTML = '<div class="row '+streamObj.status+'">'+logoHTML+streamHTML+textHTML+linkHTML+'</div>';
     $("#streamsContainer").append(rowHTML);
-    if(streamObj.status === "online"){
-      var linkSpan = '<span class="linkSpan">Watch Now</span>';
-      $("#streamsContainer>.row").append(linkSpan);
-    }
   };
 
 
