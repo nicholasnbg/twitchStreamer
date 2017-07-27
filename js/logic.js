cf07dd915cc58060bfd016d2df66a18928275a71
nbg <nicholasnbg@gmail.com>
2017-07-27 21:23:29 +1000

    commiting a working tiles layout before taking on filtering fake streams

--- js/logic.js
+++ js/logic.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
   var clientID = '?client_id=z2yjt6dgs59kua030e1m9pa1i9rifx';
   var streams_api = "https://api.twitch.tv/kraken/streams/";
   var channels = ["ESL_SC2", "OgamingSC2", "cretetion", 'fakestream154651323' ];
+  var streamData = [];
   var streamCount = 4;
   var rows = 2;
 
@@ -28,6 +29,8 @@ $(document).ready(function(){
       }
     });
   });
+console.log(streamData)
+
 
   function fetchData(data, i, name){
 
@@ -42,21 +45,30 @@ $(document).ready(function(){
 
     //offline is currently running both ifs, fix this
     if(data.stream === null){
-      streamInfo.text = " is OFFLINE";
-      streamInfo.status = "offline"
-      streamInfo.name = name;
-      streamInfo.link = data._links.channel;
-      streamInfo.linkText = '<a href="https://www.twitch.tv/'+streamInfo.name+'/videos/all" target="blank">Go to channel</a>';
-      streamInfo.logo = "https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/ProhibitionSign2.svg/1200px-ProhibitionSign2.svg.png";
-      insertStream(streamInfo,i);
-    }
-    else if(data.stream._id){
+      //check for real channel here
+
+          streamInfo.text = " is OFFLINE";
+          streamInfo.status = "offline"
+          streamInfo.name = name;
+          streamInfo.link = data._links.channel;
+          streamInfo.linkText = '<a href="https://www.twitch.tv/'+streamInfo.name+'/videos/all" target="blank">Go to channel</a>';
+          streamInfo.logo = "https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/ProhibitionSign2.svg/1200px-ProhibitionSign2.svg.png";
+          streamData[i]= streamInfo;
+          console.log('inserting data with '+streamInfo+' '+i);
+          insertStream(streamInfo,i);
+
+        }
+
+
+    if(data.stream._id){
         streamInfo.name = name;
         streamInfo.status="online";
         streamInfo.text = " is currently playing " + data.stream.game;
         streamInfo.logo =  data.stream.channel.logo;
         streamInfo.linkText = '<a href="https://www.twitch.tv/'+streamInfo.name+'" target="blank">Watch here</a>';
         streamInfo.link = data.stream._links.self;
+        streamData[i]= streamInfo;
+        console.log('inserting data with '+streamInfo+' '+i);
         insertStream(streamInfo,i);
     }
 
@@ -69,7 +81,6 @@ $(document).ready(function(){
     for(i=1;i<=reqRows;i++){
       rowsArr.push(i);
     }
-      console.log('rows req = '+ rowsArr);
     rowsArr.forEach(function(rowNum){
       var rowHTML = '<div class="row row'+rowNum+'"></div>';
       $("#streamsContainer").append(rowHTML);
@@ -78,7 +89,6 @@ $(document).ready(function(){
 
 /// TODO: Need to work out overflow, or concats for long names
   function insertStream(streamObj, index){
-    console.log('inserting stream');
     var logoHTML = '<img src="'+streamObj.logo+'" alt="">';
     var streamHTML = '<p class="streamName">'+ streamObj.name +'</p>';
     var textHTML =  '<p class="streamText">'+ streamObj.text +'</p>';
@@ -88,9 +98,32 @@ $(document).ready(function(){
     if(Math.ceil((index+1)/3)>1){
       rowPos = Math.ceil((index+1)/3);
     }
-    console.log('inserting into row '+rowPos + ' because index is '+index);
     $(".row"+rowPos).append(tileHTML);
   };
+
+  function getChannel(channel){
+    var real = true;
+    $.ajax({
+      url: channel+clientID,
+      dataType: "json",
+      data: {
+        format: "json",
+      },
+      success: function(data){
+       //what to do here to split offline real users and fake users
+        noChannel(data);
+      },
+      error: function(){
+        console.log("Couldn't access JSON in check channel"+channel + ' is no good, '+ 404);
+      }
+    });
+  }
+
+  function noChannel(data){
+    if(data.status!== 404){
+
+    }
+  }
 
   //add stream input bar functions
   $('#search-btn').click(function(){
